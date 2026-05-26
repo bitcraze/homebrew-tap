2f8585d388a4af0a81543b180f1bb16ce1a07e98
Marcus Eliasson <marcus@bitcraze.io>
2026-05-26 23:30:25 +0200

    Keep CI-managed formula; this PR only updates the tap README

--- Formula/cfcli.rb
+++ Formula/cfcli.rb
@@ -1,26 +1,26 @@
 class Cfcli < Formula
   desc "CLI tool for Crazyflie drones"
   homepage "https://github.com/bitcraze/cfcli"
-  version "0.5.0"
+  version "0.11.1"
   license "MIT OR Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bitcraze/cfcli/releases/download/0.5.0/cfcli-aarch64-apple-darwin.tar.gz"
-      sha256 "81774890865caf21d091cb9c4a4de647b3f3feef9693609c739c99fa6db6164c"
+      url "https://github.com/bitcraze/cfcli/releases/download/0.11.1/cfcli-aarch64-apple-darwin.tar.gz"
+      sha256 "bf73b7640548e4ce6fd7c46d52c64cf0f02ef80570f3f496c4e1f00b54a99f36"
     else
-      url "https://github.com/bitcraze/cfcli/releases/download/0.5.0/cfcli-x86_64-apple-darwin.tar.gz"
-      sha256 "5cd70403b9bb12fe58b85c9e54d586f331f2b06fa82f8c7da340e3a23d1c9b2b"
+      url "https://github.com/bitcraze/cfcli/releases/download/0.11.1/cfcli-x86_64-apple-darwin.tar.gz"
+      sha256 "4c4b66fa4fb4220126579855d00c8ef98120ce571b63ef59aa7eba6b7f5d333c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bitcraze/cfcli/releases/download/0.5.0/cfcli-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "38fa117eb7473d53282401279e2ce54a48584950161bf5e68a14f5e0395928bd"
+      url "https://github.com/bitcraze/cfcli/releases/download/0.11.1/cfcli-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8c4f54c62fbbce1b58118f879270bb45c193c20d7b8383901c8e55981ce9bb4d"
     else
-      url "https://github.com/bitcraze/cfcli/releases/download/0.5.0/cfcli-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "bab6e0f562c0c42530e21d3ce69de64286b5c6d35d951b619b6177fe86c6e6ff"
+      url "https://github.com/bitcraze/cfcli/releases/download/0.11.1/cfcli-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a66bfac26790b276152797d671b95976bba600044e1d5241a23485879d3e9139"
     end
   end
 
